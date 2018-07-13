6cec766de4269fbed15e62dd7c3fda42f6bb3cec
bella13 <isabellad.13@icloud.com>
2018-07-13 14:18:29 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,45 @@
 # river.rb
+
+class River 
+  
+def intialize(name)
+  @name = name 
+end 
+ 
+ def name 
+@name 
+end 
+
+def name =(name)
+  @name = name 
+end 
+
+def length 
+  @length 
+end 
+
+def length = (length)
+  @length = length
+end
+
+def countries 
+  @countries 
+end 
+
+def countries = (countries)
+@countries = countries  
+end 
+
+def discharge 
+  @discarge 
+end 
+
+def discharge = (discharge)
+  @discharge = dischrge 
+ end 
+
+end 
+  
+  
+  
+  
